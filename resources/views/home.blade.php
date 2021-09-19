9c9d85f7519f0ab7792fa37aade94847e147a036
jakub-sieradzki <rootekstudio@outlook.com>
2021-09-19 15:34:09 +0200

    Added Tailwind CSS

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>New Network</title>
+        <link href="/css/app.css" rel="stylesheet">
     </head>
     <body>
         <div id="app"></div>
