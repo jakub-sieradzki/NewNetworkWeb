d114b2ae401d66b24a268a5e29efe5f35fcd8bc9
jakub-sieradzki <rootekstudio@outlook.com>
2021-09-27 20:01:11 +0200

    Fixes to Register layout

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -12,15 +12,15 @@
 <body class="dark:bg-gray-800 h-screen w-screen">
     <div id="wave-div" class="z-0">
     </div>
-    <div class="absolute h-screen w-screen z-10 flex flex-col">
-        <header class="relative w-full h-16 flex justify-between">
+    <div class="absolute min-h-screen max-h-screen h-screen w-screen z-10 flex flex-col flex-shrink ">
+        <header class="relative w-full h-16 flex justify-between flex-shrink-0">
             <p class="ml-20 text-2xl self-end text-white">New Network</p>
             <p class="mr-14 text-2xl self-end">Rejestracja</p>
         </header>
-        <main class="flex justify-between h-full">
-            <div class=" flex flex-col ml-20 mt-28 shadow-xl rounded-2xl bg-white bg-opacity-60 dark:bg-black p-6 pr-2  overflow-y-hidden" style="width: 550px; height: 700px;">
+        <main class="relative flex flex-shrink justify-between overflow-hidden">
+            <div class="flex flex-shrink flex-col ml-20 mt-28 mb-8 shadow-xl rounded-2xl bg-white bg-opacity-60 dark:bg-black p-6 pr-4 overflow-y-hidden" style="width: 550px;">
                 <div>
-                    <p class="text-5xl text-center">KONSTYTUCJA</p>
+                    <p class="text-5xl text-center">ZASADY MODERACJI</p>
                     <p class="text-2xl text-center mt-2">New Network</p>
                 </div>
                 <div class="mt-6 overflow-y-scroll custom-scrollbar-register-rules pr-4">
@@ -71,100 +71,106 @@
                     </div>
                 </div>
             </div>
-            <div class="flex flex-shrink">
-                <div class="bg-gray-100 dark:bg-gray-900 mr-14 mt-7 flex-shrink rounded-xl shadow-2xl border dark:border-gray-800 overflow-y-scroll custom-scrollbar" style="height: 800px; width: 900px;">
-                    <div class="ml-10 mr-5 pr-5 h-full">
-                        <p class="text-2xl p-5 pl-0 border-b-2 dark:border-gray-800">Dane logowania</p>
-                        <div class="flex justify-between">
-                            <div>
-                                <label class="label">
-                                    <span class="label-text">Email</span>
-                                </label>
-                                <input type="email" placeholder="jan.kowalski@example.com" class="input input-bordered dark:bg-gray-800 w-96">
-                                <label class="label">
-                                    <span class="label-text">Hasło</span>
-                                </label>
-                                <input type="password" placeholder="Hasło" class="input input-bordered dark:bg-gray-800 w-96">
-                                <label class="label">
-                                    <span class="label-text">Powtórz hasło</span>
-                                </label>
-                                <input type="password" placeholder="Hasło" class="input input-bordered dark:bg-gray-800 w-96">
-                            </div>
-                            <div class="flex flex-col">
-                                <div class="w-80 bg-gray-200 dark:bg-gray-800 mt-9 rounded-lg text-xs p-5">
-                                    <p>Zalecamy rejestrację konta na email założony u <a href="" class="link">zaufanego dostawcy email</a>. Jeżeli podasz adres od innego dostawcy, poprosimy Cię o numer telefonu.</p>
-                                    <br>
-                                    <p>Po wpisaniu hasła sprawdzimy, czy nie zostało ono upublicznione w wyciekach danych. Zalecamy korzystanie z menedżerów haseł, takich jak <a href="https://bitwarden.com" target="_blank" class="link">Bitwarden</a> i wbudowanych w nich generatorów haseł.</p>
-                                </div>
+            <div class="bg-gray-100 dark:bg-gray-900 relative flex-shrink h-auto mr-14 mt-7 mb-8 shadow-lg rounded-xl border dark:border-gray-800 overflow-y-scroll custom-scrollbar" style="width: 900px;">
+                <div class="ml-10 mr-5 pr-5">
+                    <p class="text-2xl p-5 pl-0 border-b-2 dark:border-gray-800">Dane logowania</p>
+                    <div class="flex justify-between">
+                        <div>
+                            <label class="label">
+                                <span class="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="jan.kowalski@example.com" class="input input-bordered dark:bg-gray-800 w-96">
+                            <label class="label">
+                                <span class="label-text">Hasło</span>
+                            </label>
+                            <input type="password" placeholder="Hasło" class="input input-bordered dark:bg-gray-800 w-96">
+                            <label class="label">
+                                <span class="label-text">Powtórz hasło</span>
+                            </label>
+                            <input type="password" placeholder="Hasło" class="input input-bordered dark:bg-gray-800 w-96">
+                        </div>
+                        <div class="flex flex-col">
+                            <div class="w-80 bg-gray-200 dark:bg-gray-800 mt-9 rounded-lg text-xs p-5">
+                                <p>Zalecamy rejestrację konta na email założony u <a href="" class="link">zaufanego dostawcy email</a>. Jeżeli podasz adres od innego dostawcy, poprosimy Cię o numer telefonu.</p>
+                                <br>
+                                <p>Po wpisaniu hasła sprawdzimy, czy nie zostało ono upublicznione w wyciekach danych. Zalecamy korzystanie z menedżerów haseł, takich jak <a href="https://bitwarden.com" target="_blank" class="link">Bitwarden</a> i wbudowanych w nich generatorów haseł.</p>
                             </div>
                         </div>
-                        <p class="text-2xl p-5 pl-0 border-b-2 dark:border-gray-800">Dane osobowe</p>
-                        <div class="flex justify-between">
-                            <div>
-                                <label class="label">
-                                    <span class="label-text">Imię</span>
-                                </label>
-                                <input type="text" placeholder="Jan" class="input input-bordered dark:bg-gray-800 w-96">
-                                <label class="label">
-                                    <span class="label-text">Nazwisko</span>
-                                </label>
-                                <input type="text" placeholder="Kowalski" class="input input-bordered dark:bg-gray-800 w-96">
-                                <label class="label">
-                                    <span class="label-text">Nazwa użytkownika</span>
-                                </label>
-                                <input type="text" placeholder="@jankowalski" class="input input-bordered dark:bg-gray-800 w-96">
-                                <label class="label">
-                                    <span class="label-text">Płeć</span>
-                                </label>
-                                <select class="select select-bordered dark:bg-gray-800 w-96">
-                                    <option disabled="disabled" selected="selected">Nie wybrano</option>
-                                    <option>Kobieta</option>
-                                    <option>Mężczyzna</option>
-                                    <option>Nie chcę podawać</option>
-                                </select>
-                                <label class="label">
-                                    <span class="label-text">Rok urodzenia</span>
-                                </label>
-                                <input type="number" type="number" min="1900" max="2099" placeholder="np. 2002" class="input input-bordered dark:bg-gray-800 w-96">
-                                <div class="p-3 card w-96 border border-gray-300 dark:border-gray-700 rounded-lg mt-7">
-                                    <div class="form-control">
-                                        <label class="cursor-pointer label">
-                                            <span class="label-text pr-3">Zapoznałem/am się i akceptuję <a href="" class="link">Warunki użytkowania</a></span>
-                                            <input type="checkbox" class="checkbox">
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="p-3 card w-96 border border-gray-300 dark:border-gray-700 rounded-lg mt-3">
-                                    <div class="form-control">
-                                        <label class="cursor-pointer label">
-                                            <span class="label-text pr-3">Zapoznałem/am się i akceptuję <a href="" class="link">Politykę prywatności</a></span>
-                                            <input type="checkbox" class="checkbox">
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="h-5"></div>
-                                <button class="btn w-96">Zarejestruj się</button>
-                            </div>
-                            <div class="flex flex-col">
-                                <div class="w-80 bg-gray-200 dark:bg-gray-800 mt-9 rounded-lg text-xs p-5">
-                                    <p>Możesz zmienić swoje imię i nazwisko w każdej chwili w ustawieniach profilu.</p>
-                                    <br>
-                                    <p>Nazwa użytkownika jest unikatowa dla każdego użytkownika i nie może być zmieniona.</p>
-                                    <br>
-                                    <p>Dozwolne znaki w nazwie użytkownika to litery (wielkość liter nie jest rozróżniana), cyfry oraz '_' i '-'.</p>
-                                    <br>
-                                    <p>Nie obsługujemy użytkowników poniżej 13 roku życia.</p>
-                                    <br>
-                                    <p>Zachęcamy do zapoznania się ze szczegółowymi zasadami moderacji (wraz z przykładami), które są dostępne <a href="" class="link">tutaj</a>.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="h-10"></div>
                     </div>
+                    <p class="text-2xl p-5 pl-0 border-b-2 dark:border-gray-800">Dane osobowe</p>
+                    <div class="flex justify-between">
+                        <div>
+                            <label class="label">
+                                <span class="label-text">Imię</span>
+                            </label>
+                            <input type="text" placeholder="Jan" class="input input-bordered dark:bg-gray-800 w-96">
+                            <label class="label">
+                                <span class="label-text">Nazwisko</span>
+                            </label>
+                            <input type="text" placeholder="Kowalski" class="input input-bordered dark:bg-gray-800 w-96">
+                            <label class="label">
+                                <span class="label-text">Nazwa użytkownika</span>
+                            </label>
+                            <input type="text" placeholder="@jankowalski" class="input input-bordered dark:bg-gray-800 w-96">
+                            <label class="label">
+                                <span class="label-text">Płeć</span>
+                            </label>
+                            <select class="select select-bordered dark:bg-gray-800 w-96">
+                                <option disabled="disabled" selected="selected">Nie wybrano</option>
+                                <option>Kobieta</option>
+                                <option>Mężczyzna</option>
+                                <option>Nie chcę podawać</option>
+                            </select>
+                            <label class="label">
+                                <span class="label-text">Rok urodzenia</span>
+                            </label>
+                            <input type="number" type="number" min="1900" max="2099" placeholder="np. 2002" class="input input-bordered dark:bg-gray-800 w-96">
+                            <div class="p-3 card w-96 border border-gray-300 dark:border-gray-700 rounded-lg mt-7">
+                                <div class="form-control">
+                                    <label class="cursor-pointer label">
+                                        <span class="label-text pr-3">Oświadczam, że mam ukończone 13 lat</span>
+                                        <input type="checkbox" class="checkbox">
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="p-3 card w-96 border border-gray-300 dark:border-gray-700 rounded-lg mt-3">
+                                <div class="form-control">
+                                    <label class="cursor-pointer label">
+                                        <span class="label-text pr-3">Zapoznałem/am się i akceptuję <a href="" class="link">Warunki użytkowania</a></span>
+                                        <input type="checkbox" class="checkbox">
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="p-3 card w-96 border border-gray-300 dark:border-gray-700 rounded-lg mt-3">
+                                <div class="form-control">
+                                    <label class="cursor-pointer label">
+                                        <span class="label-text pr-3">Zapoznałem/am się i akceptuję <a href="" class="link">Politykę prywatności</a></span>
+                                        <input type="checkbox" class="checkbox">
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="h-5"></div>
+                            <button class="btn w-96">Zarejestruj się</button>
+                        </div>
+                        <div class="flex flex-col">
+                            <div class="w-80 bg-gray-200 dark:bg-gray-800 mt-9 rounded-lg text-xs p-5">
+                                <p>Możesz zmienić swoje imię i nazwisko w każdej chwili w ustawieniach profilu.</p>
+                                <br>
+                                <p>Nazwa użytkownika jest unikatowa dla każdego użytkownika i nie może być zmieniona.</p>
+                                <br>
+                                <p>Dozwolne znaki w nazwie użytkownika to litery (wielkość liter nie jest rozróżniana), cyfry oraz '_' i '-'.</p>
+                                <br>
+                                <p>Nie obsługujemy użytkowników poniżej 13. roku życia.</p>
+                                <br>
+                                <p>Zachęcamy do zapoznania się ze szczegółowymi zasadami moderacji (wraz z przykładami), które są dostępne <a href="" class="link">tutaj</a>.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="h-10"></div>
                 </div>
             </div>
         </main>
-        <footer class="flex h-24 flex-row-reverse items-center text-sm">
+        <footer class="flex h-12 flex-row-reverse items-start text-sm flex-shrink-0">
             <a href="index.html" class="mr-14 link">
                 <p class="">O nas</p>
             </a>
